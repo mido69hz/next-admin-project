c79eddeef415391fac79fb9a607d9ba2a803290a
= <=>
2024-09-16 17:16:16 +0800

    69

--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -4,10 +4,20 @@ import { Button } from "@/components/ui/button";
 import { TypographyH3 } from "@/components/typography/h3";
 import { UsersTable } from "./table";
 import { UserCreateDialog } from "./user-create-dialog";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Users = () => {
+  const [data, setData] = useState([]);
   const [createModalOpen, setCreateModalOpen] = useState(false);
+
+  useEffect(() => {
+    fetch("/api/users")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
+
   return (
     <div>
       <Card>
@@ -20,7 +30,13 @@ const Users = () => {
           </div>
         </CardHeader>
         <CardContent>
-          <UsersTable />
+          {data.length > 0 && <UsersTable data={data} />}
+          <div
+            className="p-8 flex justify-center
+          "
+          >
+            <button variant="outline">Load more...</button>
+          </div>
         </CardContent>
       </Card>
 
